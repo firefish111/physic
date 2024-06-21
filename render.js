62d2ffb2dc36d18144e48ae3b19b266b47d38d2f
school <mailsodd@gmail.com>
2024-06-21 13:06:57 +0100

    Push somewhat working physics code to github

---
+++ render.js
@@ -0,0 +1,82 @@
+const SCALE = 100; // px = cm
+const GRAVITY = 9.81; // 9.81 m/s^2
+/**
+ * @method putShape Initialise shape for drawing to screen
+ * @method drawShape Draw shape to screen
+ * @method tick Executed every frame, (every 1/TPS seconds), to move the shapes
+ */
+class Render {
+  constructor(cv) { // init func
+    this.canv = cv;
+    this.rend = this.canv.getContext("2d");
+
+    this.TPS = 60; // ticks per second (tick === frame)
+
+    this.shp_lst = [];
+  }
+
+  tick() {
+    this.rend.clearRect(0, 0, this.canv.width, this.canv.height);
+
+    this.shp_lst.forEach(k => {
+      if (!k.style.movable) return;
+
+      let [x, y] = k.bottom_point();
+      let strk = k.style.strokeWidth;
+      if (x <= this.canv.width && y <= this.canv.height) {
+        k.velocity[1] += GRAVITY * SCALE / this.TPS; // 9.81/60, to convert m/s^2 into px/t^2
+        k.translate(
+          k.velocity[0] / this.TPS, // velocity is per second, so we make it per tick
+          k.velocity[1] / this.TPS,
+          this.canv.width - strk/2, // half stroke width, as centre of stroke line is edge of shape
+          this.canv.height - strk/2, // this stops floating shapes
+        );
+
+        if (y == this.canv.height) { // if on floor
+          k.bounce()
+        }
+      }
+
+    });
+    this.drawShape(this.shp_lst);
+  }
+
+  drawShape(...shps) {
+    ((shps.length === 1 && shps[0] instanceof Array) ? shps[0] : shps).forEach(shp => {
+      shp.path = new Path2D();
+      shp.setpath(...shp.store);
+
+      this.rend.fillStyle = shp.style.fill ?? "transparent";
+      this.rend.fill(shp.path);
+
+      this.rend.lineWidth = shp.style.strokeWidth ?? 0;
+      this.rend.strokeStyle = shp.style.stroke ?? 0;
+      this.rend.stroke(shp.path);
+    });
+  }
+
+  putShape(...shps) {
+    ((shps.length === 1 && shps[0] instanceof Array) ? shps[0] : shps).forEach(shp => {
+      if (!(shp instanceof Shape)) { throw "not a shape"; };
+
+      this.shp_lst.push(shp);
+
+      this.drawShape(shp);
+    });
+  }
+}
+
+const rd = new Render(document.querySelector("canvas#canv"));
+let rect = new Rectangle({ fill: "orange", stroke: "black", strokeWidth: 8 }, 100, 20, 210, 85);
+let circ = new Circle({ fill: "blue", stroke: "#a4db0b", strokeWidth: 5 }, 500, 200, 25);
+let lin = new Line({ stroke: "#a952b9", strokeWidth: 10 }, 460, 280, 330, 120);
+let lin2 = new Line({ stroke: "#cd2246", strokeWidth: 5 }, 530, 350, 580, 60);
+
+// dynamically pass the canvas, as the canvas' dimensions are tied to the viewport
+let floor = new CanvasBorder({ stroke: "black", strokeWidth: 1 }, rd.canv);
+
+rd.putShape(floor, rect, circ, lin, lin2);
+
+window.setInterval(() => {
+  rd.tick();
+}, 1000 / rd.TPS);
---
+++ shapes.js
@@ -0,0 +1,210 @@
+/**
+ * @property {string} style style object
+ * @property {Array<any>} store argstore
+ * 
+ * @method setpath Function to define path of shape being traced
+ * @method capout Jank max function
+ */
+class Shape {
+  constructor(fill, ...args) { 
+    this.style = fill;
+    this.style.movable ??= true;
+    console.log(this.style.movable);
+    this.path = new Path2D();
+
+    // tomfoolery
+    //this.style.strokeWidth = 1e-308;
+
+    this.velocity = [0, 0];
+
+    // arguments to setpath function are stored as an array, then called every tick
+    // this means that if a shape is a vararg (like a polygon) it will still work
+    // and that constructor typesig doesn't have to be changed with each shape, only setpath
+    this.store = args; // argstore
+    this.setpath(...this.store);
+
+    this.ROTATIONAL_SCALE = 80;
+    
+    // bounce 1 time means it bounces high, bounce 2 times and it stops bouncing as high, for some reason
+    // it works like this, don't touch it whatever you do
+    this.bounces = 1;
+
+    // infinite (positive or negative) are means zero bounciness
+    this.area = -Infinity;
+  }
+
+  capout(p, val, lim) {
+    let should = p + val;
+    return Math.max(should - lim, 0);
+  }
+
+  setpath() {}
+
+  bounce() {
+    // can only bounce a limited number of times
+    // not exactly sure why adding a "<= 0" afterwards doesn't work
+    if (this.bounces--) {
+      this.velocity[1] = -this.velocity[1] / (this.area ** .09);
+    }
+  }
+}
+
+const DEGREE = Math.PI/180;
+
+class Rectangle extends Shape {
+  setpath(x1, y1, x2, y2) {
+    this.path.rect(x1, y1, x2 - x1, y2 - y1);
+    this.area = (x2 - x1) * (y2 - y1);
+  }
+
+  translate(x, y, limx, limy) {
+    let capx = x - Math.max(
+      this.capout(this.store[0], x, limx),
+      this.capout(this.store[2], x, limx)
+    );
+    let capy = y - Math.max(
+      this.capout(this.store[1], y, limy),
+      this.capout(this.store[3], y, limy)
+    );
+
+    this.store[0] = Math.min(this.store[0] + capx, limx);
+    this.store[1] = Math.min(this.store[1] + capy, limy);
+    this.store[2] = Math.min(this.store[2] + capx, limx);
+    this.store[3] = Math.min(this.store[3] + capy, limy);
+  }
+
+  bottom_point() {
+    return [
+      Math.max(this.store[0], this.store[2]) + this.style.strokeWidth / 2,
+      Math.max(this.store[1], this.store[3]) + this.style.strokeWidth / 2
+    ];
+  }
+
+  rotate() { 
+    // nothing
+    // rotation of a rectangle is difficult, because of it becomes not parallel to the floor, it is no longer a rectangle, and it becomes a polygon instead
+    // i don't feel like making shapes that mutate
+  }
+}
+
+// 1px wide black border, to see ground
+class CanvasBorder extends Rectangle {
+  setpath(canv) {
+    this.path.rect(0, 0, canv.width, canv.height);
+  }
+}
+
+class Line extends Shape {
+  setpath(x1, y1, x2, y2) {
+    this.path.moveTo(x1, y1);
+    this.path.lineTo(x2, y2);
+    this.lowest = +(y1 < y2); // 1 if y1 lower down (more than) y2, else 0
+
+    // area is -Inf (can't be 0 as that ends in Inf, dividing by -Inf results in -0)
+  }
+
+  translate(x, y, limx, limy) {
+    let capx = x - Math.max(
+      this.capout(this.store[0], x, limx),
+      this.capout(this.store[2], x, limx)
+    );
+    let capy = y - Math.max(
+      this.capout(this.store[1], y, limy),
+      this.capout(this.store[3], y, limy)
+    );
+
+    let min_fall = (a, b) => a < b ? a : (this.fall_by_rotate(9.81) || b)
+    this.store[0] = min_fall(this.store[0] + capx, limx);
+    this.store[1] = min_fall(this.store[1] + capy, limy);
+    this.store[2] = min_fall(this.store[2] + capx, limx);
+    this.store[3] = min_fall(this.store[3] + capy, limy);
+  }
+
+  bottom_point() {
+    return [
+      Math.max(this.store[0], this.store[2]) + this.style.strokeWidth / 2,
+      Math.max(this.store[1], this.store[3]) + this.style.strokeWidth / 2
+    ];
+  }
+
+  rotate(theta, which = 0) {
+    // x' = xcos(theta) - ysin(theta)
+    // y' = xsin(theta) + ycos(theta)
+    
+    // which is pointnum, 0 is x1-x0, 1 is x0-x1
+    let Dx = this.store[2] - this.store[0];
+    let Dy = this.store[3] - this.store[1]; // originified towards top-left
+
+    if (which) {
+      this.store[0] = this.store[2] - Dx * Math.cos(theta) + Dy * Math.sin(theta);
+      this.store[1] = this.store[3] - Dx * Math.sin(theta) - Dy * Math.cos(theta);
+    } else {
+      this.store[2] = this.store[0] + Dx * Math.cos(theta) - Dy * Math.sin(theta);
+      this.store[3] = this.store[1] + Dx * Math.sin(theta) + Dy * Math.cos(theta);
+    }
+  }
+
+  fall_by_rotate(g) {
+    // get most southernly point
+    let which = +(this.store[1] < this.store[3]); // to compare against the point that we have been falling already, to prevent cartwheeling
+    if (which != this.lowest) return;
+
+    let Dx = Math.abs(this.store[2] - this.store[0]); // width
+    let Dy = Math.abs(this.store[3] - this.store[1]); // height
+    //let len = Math.sqrt(Dx**2 + Dy**2)
+    let angle = Math.atan(Dy/Dx);
+    //angle = Math.max(Math.min(angle, Math.PI*.5), 0);
+    //if (!angle) return;
+
+    let torque = Math.min((Math.PI - angle)**(2)/this.ROTATIONAL_SCALE, angle); 
+    if (
+      (this.store[0] < this.store[2] && this.store[1] < this.store[3]) ||
+      (this.store[0] > this.store[2] && this.store[1] > this.store[3])
+    ) torque *= -1; // check if needing to rotate anticlockwise instead, if p1 is either nw or se of p0
+
+    this.rotate(torque, this.lowest);
+  }
+}
+
+// arc can be applied to much the same stuff as the line
+class Arc extends Line {
+  setpath(x, y, r, angle) {
+    this.path.arc(x, y, r, 0, angle /* in radians */, angle < 0);
+    this.area = Math.PI * (r**2)
+  }
+
+  translate(x, y, limx, limy) {
+    let capx = x - this.capout(this.store[0] + this.store[2], x, limx);
+    let capy = y - this.capout(this.store[1] + this.store[2], y, limy);
+
+    this.store[0] = Math.min(this.store[0] + capx, limx);
+    this.store[1] = Math.min(this.store[1] + capy, limy);
+  }
+
+  bottom_point() {
+    return [
+      this.store[0] + this.style.strokeWidth / 2,
+      this.store[1] + this.store[2] + this.style.strokeWidth / 2
+    ];
+  }
+
+  rotate(theta) {
+    // x' = xcos(theta) - ysin(theta)
+    // y' = xsin(theta) + ycos(theta)
+    //this.store[0] = this.store[0] * Math.cos(theta) - this.store[1] * Math.sin(theta);
+    //this.store[1] = this.store[0] * Math.sin(theta) + this.store[1] * Math.cos(theta);
+  }
+}
+
+// circle is just a 360deg (2pi rad) arc
+class Circle extends Arc {
+  constructor(...args) {
+    super(...args, Math.PI * 2);
+  }
+
+  rotate() {
+    // it's a circle
+    // rotations mean nothing
+    // what do you expect
+  }
+}
